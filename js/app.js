6976acb58f17768595b78a4a66f3a5d2b7543ec9
Bradigson <71954800+Bradigson@users.noreply.github.com>
2021-08-14 22:37:15 -0400

    done

---
+++ js/app.js
@@ -0,0 +1,25 @@
+let btn = document.querySelector(".btn");
+btn.addEventListener("click", (e)=>{
+e.preventDefault();
+    let alert= document.querySelector(".alert");
+    let message = document.querySelector("p");
+    let username = document.querySelector("#username");
+    let email = document.querySelector("#email");
+    let textarea = document.querySelector("textarea");
+    if(username.value == ""){
+     alert.style.display = "flex";
+     message.textContent = "The file username can't be empty";
+    }else if(email.value == ""){
+        alert.style.display = "flex";
+        message.textContent = "The file email can't be empty";
+    }else if(textarea.value == ""){
+        alert.style.display = "flex";
+        message.textContent = "The file message can't be empty";
+    }
+});
+
+let close =  document.querySelector(".btn-cancel");
+close.onclick = function(e){
+    let alert= document.querySelector(".alert");
+    alert.style.display = "none";
+}
